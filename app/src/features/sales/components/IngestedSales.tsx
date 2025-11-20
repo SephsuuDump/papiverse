df6eaab3270a0b2e09d571f035de5fe23236770b
Joseph Bataller <batallerjem208@gmail.com>
2025-11-21 01:25:55 +0800

    fixed sales caldendar and ingestedsales table

--- app/src/features/sales/components/IngestedSales.tsx
+++ app/src/features/sales/components/IngestedSales.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
+import { useSearchFilter } from "@/hooks/use-search-filter";
 import { formatToPeso } from "@/lib/formatter";
 import { Plus, X } from "lucide-react";
 
